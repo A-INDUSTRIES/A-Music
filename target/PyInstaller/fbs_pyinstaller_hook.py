31b1faa8314e171306ab36d3d8e9aec2e0a9539e
AINDUSTRIES <alexandredaoust4@gmail.com>
2021-01-04 19:03:18 +0100

    New exe

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++ target/PyInstaller/fbs_pyinstaller_hook.py
@@ -1,3 +1,3 @@
 import importlib
 module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'A+Music', 'author': 'AINDUSTRIES', 'version': '1.4.0Pre2', 'environment': 'local'}
+module.BUILD_SETTINGS = {'app_name': 'A+Music', 'author': 'AINDUSTRIES', 'version': '2.0.0', 'environment': 'local'}

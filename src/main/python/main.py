e707baa8b3671f74acc82e5a607cae415c2144f6
AINDUSTRIES <alexandredaoust4@gmail.com>
2021-01-05 19:50:46 +0100

    Version number update

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,7 +1,7 @@
 """
 Author: DAOUST A. @AINDUSTRIES
 Project: A+Music Player
-v2.0.0
+v2.0.1
 """
 from fbs_runtime.application_context.PySide2 import ApplicationContext
 from PySide2 import QtWidgets, QtCore, QtGui, QtMultimedia

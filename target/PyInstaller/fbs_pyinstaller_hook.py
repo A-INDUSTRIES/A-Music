0c431f1766f0d9c8ee93fe890599874b44bd81bc
AINDUSTRIES <contact.aindustries@gmail.com>
2024-02-13 17:00:56 +0100

    Delete target directory

--- target/PyInstaller/fbs_pyinstaller_hook.py
+++
@@ -1,3 +0,0 @@
-import importlib
-module = importlib.import_module('fbs_runtime._frozen')
-module.BUILD_SETTINGS = {'app_name': 'A+Music', 'author': 'AINDUSTRIES', 'version': '2.1.1', 'environment': 'local'}
